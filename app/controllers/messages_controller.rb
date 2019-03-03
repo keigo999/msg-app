e0b093af9d4fc2f6ce8a552cc7c10c9db54e2065
keigo999 <highbridgek5@gmail.com>
2019-03-03 16:38:45 +0900

    vertexg機能追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,12 @@
 class MessagesController < ApplicationController
   def show
+
+  if  params[:msg] == "vertex"
+    render "vertexg"
+  end
     @msg = params[:msg]
+  end
+
+  def vertexg
   end
 end
